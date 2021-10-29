09e03008ddcf86b8863b2a053d404f0c3a7c7fa9
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-10-29 23:21:06 +0600

    update

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router';
 
 const Booking = () => {
+    const { id } = useParams();
+
+    const [bookingPackage, setBookingPackage] = useState({});
+    useEffect(() => {
+        fetch(`http://localhost:5000/booking/${id}`)
+            .then(res => res.json())
+            .then(data => setBookingPackage(data))
+    }, [])
+    const { name, description, guide, cost, img, duration } = bookingPackage;
     return (
-        <div>
-            <h2>This is Booking</h2>
+        <div className="container">
+            <h1 className="color-b text-center mt-4 mb-4">Tour Details</h1>
+            <div className="d-flex">
+                <div><img src={img} alt="" /></div>
+                <div className="px-4">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                    <p className="fw-bold">Duration: {duration}</p>
+                    <p className="fw-bold color-a">{cost}</p>
+                    <p className="fw-bold bg-warning text-center p-2">{guide} will Guide This tour</p>
+                </div>
+            </div>
         </div>
     );
 };
