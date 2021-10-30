469dfa1fd3f36cb644311f726d2ee76fb440931f
Mehedi Hasan Siam <mehedisiam0022@gmail.com>
2021-10-31 01:41:33 +0600

    update

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -7,9 +7,9 @@ const DashBoard = () => {
         <div>
             <div className="btn-popup mx-auto my-auto bg-a text-center">
 
-                <Link to="/allOrders" className="text-white nav-menu fw-bold color-a">Manage Bookings</Link>
+                <Link to="/allOrders" className=" btn btn-warning fw-bold">Manage Bookings</Link>
                 <br />
-                <Link to="/myOrders" className="text-white nav-menu fw-bold color-a">My Bookings</Link>
+                <Link to="/myOrders" className="btn btn-warning nav-menu fw-bold my-4">My Bookings</Link>
             </div>
         </div>
     );
